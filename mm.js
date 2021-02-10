d6ae0c4515e9b1e0e751c7a9539800bd2bef38dd
cdnvip <78871156+cdnvip@users.noreply.github.com>
2021-02-11 02:18:43 +0800

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,23 @@
+function js_xhnr3(){
+//toutia--origin/138ac0000aaf2330cfe0c
+document.writeln("<article class=\'article-list br mb sb clearfix\'><figure class=\'figure fl\'><a class=\'thumbnail\' target=\'_blank\' href=\'http://so.jsjst.top/choiceness?keyword=");
+document.write(add1);
+document.writeln("\' title=\'点击领取淘宝天猫优惠券\'><img class=\'img-cover br\' src=\'https://ae01.alicdn.com/kf/Ucdc57a8489b14cf09e079fa1399d08dcv.jpg\' title=\'点击领取淘宝天猫优惠券\'>");
+document.writeln("</a></figure><div class=\'content\'><h2 class=\'title ellipsis m-multi-ellipsis\'><a target=\'_blank\' href=\'http://so.jsjst.top/choiceness?keyword=");
+document.write(add1);
+document.writeln("\' title=\'点击领取淘宝天猫优惠券 \'><span class=\'istop\'>置顶</span><font color=\'#0084ff\'>");
+document.write(add1);
+document.writeln("，正在优惠中，点击领取优惠券</font></a></h2><p class=\'intro hidden-sm\'>有些事情我不说，你可能不知道...</p><p class=\'data clearfix\'><span class=\'hidden-sm-md-lg author fl\'><i class=\'iconfont icon-user\'></i>爆料</span><time class=\'time fl\' datetime=\'2021-限时优惠\' title=\'2021-限时优惠\'><i class=\'iconfont icon-time\'></i>2021 限时优惠</time><span class=\'view fl\'><i class=\'iconfont icon-view\'></i>7951</span><span class=\'hidden-sm-md-lg tag ellipsis fr\'></span></p></div></article>");
+}
+
+function js_xhnr4(){
+document.writeln("<a href=\'https://curl.qcloud.com/13mYRXaV\' target=\'_blank\'><img style=\'max-width:100%;\' src=\'https://p.pstatp.com/origin/137ee0002af0412477afd\'></a>");
+}
+
+function js_xhnr6(){
+document.writeln("<section id=\'aside_ad\' class=\'widget widget_aside_ad sb br mb\'><div class=\'widget-content aside_ad\'><a href=\'http://www.xuhangzj.com/tuijian/1064.html\' rel=\'nofollow\' target=\'_blank\'><img class=\'br\' src=\'https://p.pstatp.com/origin/1382d00032a29edc4e971\' alt=\'推荐\'/></a></div></section>");
+}
+
+function js_xhsy4(){
+document.writeln("<section id=\'aside_ad\' class=\'widget widget_aside_ad sb br mb\'><div class=\'widget-content aside_ad\'><a href=\'http://www.xuhangzj.com/tuijian/1064.html\' rel=\'nofollow\' target=\'_blank\'><img class=\'br\' src=\'https://p.pstatp.com/origin/1382d00032a29edc4e971\' alt=\'推荐\'/></a></div></section>");
+}
